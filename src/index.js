be709e9fad3cb99855558d0410fc6a8e56109ece
Anna Shatilova <anxet@mail.ru>
2023-11-05 00:06:53 +0300

    Добавлены остальные обработчики и комментарии

--- src/index.js
+++ src/index.js
@@ -1,33 +1,58 @@
-const { log } = require("console");
 const http = require("http");
-const getUsers = require("./modules/users")
+// Импортируем класс URL из встроенного модуля url
+const { URL } = require("url");
+const getUsers = require("./modules/users");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+// создание сервера происходит при помощи функции createServer
 
 const server = http.createServer((request, response) => {
-  if (request.url === "/users") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+  const url = new URL(request.url, `http://${hostname}`);
+  const query = url.searchParams;
+
+  // запрос на ?hello=<name>
+
+  if (query.has("hello")) {
+    const name = query.get("hello");
+
+    if (name) {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.write(`Hello, ${name}.`);
+      response.end();
+      return;
+    }
+
+    response.statusCode = 400;
+    response.setHeader("Content-Type", "text/plain");
+    response.write("Enter a name");
+    response.end();
+    return;
+  } else if (url.pathname === "/users") {
+    // запрос для /users
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
     response.write(getUsers());
     response.end();
-
+    return;
+  } else if (url.search === "") {
+    // если никакие параметры не переданы
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.write("Hello, World!");
+    response.end();
+    return;
+  } else {
+    response.statusCode = 500;
+    response.end();
     return;
   }
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
-  response.write("Hello, World!");
-  response.end();
-
-  // Написать обработчик запроса:
-  // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-  // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-  // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-  // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-  // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
 });
 
 // запуск сервера выполняется командой .listen
 
 server.listen(3000, () => {
-  console.log("Сервер запущен по адресу http://127.0.0.1:3000");
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
