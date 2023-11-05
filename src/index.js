0e2153c60bf155a850b4a5dc3aba59ce5cf7cf7d
Anna Shatilova <anxet@mail.ru>
2023-11-05 16:27:04 +0300

    Использована переменная при запуске сервера

--- src/index.js
+++ src/index.js
@@ -53,6 +53,6 @@ const server = http.createServer((request, response) => {
 
 // запуск сервера выполняется командой .listen
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
